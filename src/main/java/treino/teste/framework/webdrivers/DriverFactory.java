85021268b5e57692cd2a2ee3306bcbcc983559fc
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2021-02-10 18:36:58 -0300

    Modifiquei nomes metodos e adicionei metodo para janela maximizar

--- src/main/java/treino/teste/framework/webdrivers/DriverFactory.java
+++ src/main/java/treino/teste/framework/webdrivers/DriverFactory.java
@@ -3,16 +3,21 @@ package treino.teste.framework.webdrivers;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
-    public static WebDriver getInstance(Drivers tipos) {
+
+    public static WebDriver driver;
+
+    public static WebDriver getBrowser(Drivers tipos) {
         switch (tipos) {
             case CHROME:
                 WebDriverManager.chromedriver().setup();
-
-                return new ChromeDriver();
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--start-maximized");
+                return driver = new ChromeDriver(options);
             case FIREFOX:
                 WebDriverManager.firefoxdriver().setup();
                 return new FirefoxDriver();
