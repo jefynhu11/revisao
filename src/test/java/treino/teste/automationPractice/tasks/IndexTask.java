cff8f01abbbcd2302bdada8aeb8dce405e6a3f65
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2021-02-10 18:28:57 -0300

    Adicionei metodo fazer bordo no elemento

--- src/test/java/treino/teste/automationPractice/tasks/IndexTask.java
+++ src/test/java/treino/teste/automationPractice/tasks/IndexTask.java
@@ -2,15 +2,19 @@ package treino.teste.automationPractice.tasks;
 
 import org.openqa.selenium.WebDriver;
 import treino.teste.automationPractice.pageobjects.IndexPage;
+import treino.teste.framework.tools.JsExecutor;
 
 public class IndexTask {
+    private final WebDriver driver;
     private final IndexPage indexPage;
 
     public IndexTask(WebDriver driver){
+        this.driver = driver;
         indexPage = new IndexPage(driver);
     }
 
     public void escolherProduto(){
+        JsExecutor.highLightElement(driver, indexPage.getProductButton());
         indexPage.getProductButton().click();
     }
 
