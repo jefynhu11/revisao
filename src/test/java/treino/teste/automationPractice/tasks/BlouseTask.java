af0eecd7ea95a9c2fc41db6958aa71dd78ff54ac
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2021-02-10 18:28:04 -0300

    Adicionei metodos fazer bordos nos elementos

--- src/test/java/treino/teste/automationPractice/tasks/BlouseTask.java
+++ src/test/java/treino/teste/automationPractice/tasks/BlouseTask.java
@@ -2,18 +2,23 @@ package treino.teste.automationPractice.tasks;
 
 import org.openqa.selenium.WebDriver;
 import treino.teste.automationPractice.pageobjects.BlousePage;
+import treino.teste.framework.tools.JsExecutor;
 
 public class BlouseTask {
 
+    private final WebDriver driver;
     private final BlousePage blousePage;
 
     public BlouseTask(WebDriver driver) {
+        this.driver = driver;
         blousePage = new BlousePage(driver);
     }
 
     public void adicionaCarrinho() throws InterruptedException {
         blousePage.getIframe();
+        JsExecutor.highLightElement(driver, blousePage.getAddToCartButton());
         blousePage.getAddToCartButton().click();
+        JsExecutor.highLightElement(driver, blousePage.getProceedToCheckoutButton());
         blousePage.getProceedToCheckoutButton().click();
     }
 }
