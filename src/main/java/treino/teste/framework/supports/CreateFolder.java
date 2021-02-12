69c23820ccab24af94176c57fa4670b6d4f6fe46
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2021-02-12 15:06:05 -0300

    Criei metodo caminho se existe pasta ou nao existe

--- src/main/java/treino/teste/framework/supports/CreateFolder.java
+++ src/main/java/treino/teste/framework/supports/CreateFolder.java
@@ -1,4 +1,14 @@
 package treino.teste.framework.supports;
 
+import java.io.File;
+
 public class CreateFolder {
+
+    public static void createDirectory(String caminho) {
+        File directory = new File(caminho);
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
+    }
+
 }
