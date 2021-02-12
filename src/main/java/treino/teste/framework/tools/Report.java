e3853ffd63bd3d8c6f560e640be0bedd07cc8e89
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2021-02-12 14:56:29 -0300

    Adicionei os codigos para gerar report

--- src/main/java/treino/teste/framework/tools/Report.java
+++ src/main/java/treino/teste/framework/tools/Report.java
@@ -1,4 +1,61 @@
 package treino.teste.framework.tools;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import org.openqa.selenium.WebDriver;
+import treino.teste.framework.supports.CreateFolder;
+
+import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Report {
+
+    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+    private static final String CAMINHO_REPORT = System.getProperty("user.dir") + File.separator + "Report";
+    public static WebDriver driver;
+    public static ExtentHtmlReporter htmlReporter;
+    public static ExtentReports extentReports;
+    public static ExtentTest extentTest;
+
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MMM-yyyy-hh'h'mm'm's's'");
+    private static final LocalDateTime localDateTime = LocalDateTime.now();
+    private static final String ldtString = dateTimeFormatter.format(localDateTime);
+
+    private static final String REPORT_NAME = "Report_" + ldtString + ".html";
+    private static final String REPORT_PATH = System.getProperty("user.dir") + File.separator + "reports" + File.separator + REPORT_NAME;
+
+    public static void configurarExtentReport(){
+        CreateFolder.createDirectory(CAMINHO_REPORT);
+
+        htmlReporter = new ExtentHtmlReporter(CAMINHO_REPORT + File.separator + REPORT_NAME);
+
+        htmlReporter.config().setDocumentTitle("Automação de web");
+        htmlReporter.config().setReportName("Automação de teste");
+        htmlReporter.config().setEncoding("UTF-8");
+        htmlReporter.config().setTheme(Theme.DARK);
+        htmlReporter.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a '('zzz')'");
+        htmlReporter.config().setJS("$('.brand-logo').html('<img src=\\\"https://i.imgur.com/qDYwkD2.png\\\" class=\\\"db-logo\\\"/>');");
+        htmlReporter.config().setCSS(".nav-wrapper { background-color: #1E317A !important; } .brand-logo { background-color: #1E317A !important; padding: 0 10px 0 0 !important; margin: 0 !important; position: absolute !important } .report-name { margin-left: 80px !important } .blue.darken-3 { background-color: #1E317A !important; color: #FFF !important;  }");
+
+        extentReports = new ExtentReports();
+
+        extentReports.attachReporter(htmlReporter);
+    }
+
+    public static void createTest(String testName){
+        extentTest = extentReports.createTest(testName);
+        test.set(extentTest);
+    }
+
+    public static void closeReport(){
+        extentReports.flush();
+        if (driver!=null){
+            driver.quit();
+            driver = null;
+        }
+    }
+
 }
