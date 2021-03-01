3c6ae340eb4a2ff2a29d63f2542e51998d202c6f
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2021-03-01 12:33:58 -0300

    Refatorar os codigos parte jsexecutor

--- src/test/java/treino/teste/automationPractice/tasks/CriarContaTask.java
+++ src/test/java/treino/teste/automationPractice/tasks/CriarContaTask.java
@@ -35,12 +35,8 @@ public class CriarContaTask {
             JsExecutor.highLightElement(driver, criarContaPage.getCreateAnAccountText());
             Assertions.assertEquals("CREATE AN ACCOUNT", criarContaPage.getCreateAnAccountText().getText());
             Report.extentTest.log(Status.PASS, "A pagina de login e parte cria a conta está correto", Screenshot.screenshotBase64(driver));
-//        criarContaPage.getCreateContaEmailFieldText().sendKeys(faker.internet().emailAddress());
-            String email = fakers.getEmail();
-            JsExecutor.highLightElement(driver, criarContaPage.getCreateContaEmailFieldText());
-            criarContaPage.getCreateContaEmailFieldText().sendKeys(email);
-            JsExecutor.highLightElement(driver, criarContaPage.getCreateAnAccountButton());
-            criarContaPage.getCreateAnAccountButton().click();
+            JsExecutor.highLightElementWithSend(driver, criarContaPage.getCreateContaEmailFieldText(), fakers.getEmail());
+            JsExecutor.highLightElementWithClick(   driver, criarContaPage.getCreateAnAccountButton());
         } catch (Exception e) {
             Report.extentTest.log(Status.ERROR, "A pagina de login e parte cria a conta não está correto", Screenshot.screenshotBase64(driver));
         }
@@ -49,21 +45,12 @@ public class CriarContaTask {
     private void cadastrarDados() {
 
         try{
-            String text = criarContaPage.getYourPersonalInformationText().getText();
             JsExecutor.highLightElement(driver, criarContaPage.getYourPersonalInformationText());
-            Assertions.assertEquals("YOUR PERSONAL INFORMATION", text);
-            if(text.equals("YOUR PERSONAL INFORMATION")){
-                System.out.println("OK");
-            } else {
-                System.out.println("FAIL");
-            }
+            Assertions.assertEquals("YOUR PERSONAL INFORMATION", criarContaPage.getYourPersonalInformationText().getText());
             Report.extentTest.log(Status.PASS, "O titulo da cadastrar está correto", Screenshot.screenshotBase64(driver));
-            criarContaPage.getTitleMrCheckBox().click();
-            JsExecutor.highLightElement(driver, criarContaPage.getFirstNameFieldText());
-            criarContaPage.getFirstNameFieldText().sendKeys(fakers.getFirstName());
-
-            JsExecutor.highLightElementTest(driver, criarContaPage.getLastNameFieldText());
-            criarContaPage.getLastNameFieldText().sendKeys(fakers.getLastName());
+            JsExecutor.highLightElementWithClick(driver, criarContaPage.getTitleMrCheckBox());
+            JsExecutor.highLightElementWithSend(driver, criarContaPage.getFirstNameFieldText(), fakers.getFirstName());
+            JsExecutor.highLightElementWithSend(driver, criarContaPage.getLastNameFieldText(), fakers.getLastName());
         } catch (Exception e) {
             Report.extentTest.log(Status.ERROR, "O titulo da cadastrar não está correto", Screenshot.screenshotBase64(driver));
         }
