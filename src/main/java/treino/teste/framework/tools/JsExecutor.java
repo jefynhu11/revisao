fd48f047d093154019587678772e25c31c7bb487
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2021-03-01 12:30:55 -0300

    Adicionei mais metodo para borda com clica e borda com digita

--- src/main/java/treino/teste/framework/tools/JsExecutor.java
+++ src/main/java/treino/teste/framework/tools/JsExecutor.java
@@ -9,12 +9,18 @@ public class JsExecutor {
         scriptElement(driver, element, HighLight.RED.getArgument());
     }
 
-    public static void highLightElementTest(WebDriver driver, WebElement element){
-        scriptElement(driver, element, HighLight.RED.getArgument());
-    }
-
     public static void highLightSelect(WebDriver driver, Select select){
         scriptSelect(driver, select, HighLight.RED.getArgument());
+    }
+
+    public static void highLightElementWithSend(WebDriver driver, WebElement element, String value){
+        scriptElement(driver, element, HighLight.RED.getArgument());
+        element.sendKeys(value);
+    }
+
+    public static void highLightElementWithClick(WebDriver driver, WebElement element){
+        scriptElement(driver, element, HighLight.RED.getArgument());
+        element.click();
     }
 
     private static void scriptElement(WebDriver driver, WebElement element, String script){
